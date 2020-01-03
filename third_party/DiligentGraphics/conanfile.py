a4ba05ba1f1a1cc8c1449e55252159e73795c17b
ReanGD <reangd@yandex.ru>
2020-01-03 21:33:33 +0300

    new version of DiligentGraphics

--- third_party/DiligentGraphics/conanfile.py
+++ third_party/DiligentGraphics/conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class DiligentGraphics(ConanFile):
     name = "DiligentGraphics"
-    version = "2.4.930577d.4a0ad96"
+    version = "2.4.7685592.1ad5f15"
     license = "Apache License 2.0"
     url = "https://github.com/DiligentGraphics/DiligentGraphics"
     description = " A modern cross-platform low-level graphics library and rendering framework"
@@ -17,12 +17,12 @@ class DiligentGraphics(ConanFile):
     def source(self):
         os.mkdir("DiligentGraphics")
 
-        core_version = "930577d"
+        core_version = "7685592"
         self.run("git clone --recursive https://github.com/DiligentGraphics/DiligentCore.git DiligentGraphics/DiligentCore")
         with tools.chdir("DiligentGraphics/DiligentCore"):
             self.run("git reset --hard %s" % core_version)
 
-        tools_version = "4a0ad96"
+        tools_version = "1ad5f15"
         self.run("git clone --recursive https://github.com/DiligentGraphics/DiligentTools.git DiligentGraphics/DiligentTools")
         with tools.chdir("DiligentGraphics/DiligentTools"):
             self.run("git reset --hard %s" % tools_version)
@@ -46,14 +46,7 @@ class DiligentGraphics(ConanFile):
         cmake = CMake(self)
         cmake.configure(source_folder="DiligentGraphics")
         cmake.install()
-        self.copy("*.h", src="DiligentGraphics/DiligentTools/NativeApp/include/",
-            dst="headers/DiligentTools/NativeApp/include/",
-            excludes=("Android", "IOS", "MacOS", "UWP", "Win32"))
-        self.copy("*.h", src="DiligentGraphics/DiligentTools/NativeApp/Linux/xcb_keysyms/",
-            dst="headers/DiligentTools/NativeApp/Linux/xcb_keysyms/")
 
-        self.copy("*.a", src="DiligentTools/NativeApp/Linux", dst="lib", keep_path=False)
-        self.copy("*.a", src="DiligentTools/NativeApp", dst="lib", keep_path=False)
         self.copy("libDiligentCore.a", src="DiligentCore", dst="lib", keep_path=False)
         self.copy("*.a", src="DiligentCore/Common", dst="lib", keep_path=False)
         self.copy("*.a", src="DiligentCore/Graphics/GraphicsTools", dst="lib", keep_path=False)
