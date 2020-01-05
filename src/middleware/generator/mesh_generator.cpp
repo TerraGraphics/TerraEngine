88bf09be5fdd7c51168e828ccb0d67b26871474c
ReanGD <reangd@yandex.ru>
2020-01-05 22:57:51 +0300

    fix mesh generator

--- src/middleware/generator/mesh_generator.cpp
+++ src/middleware/generator/mesh_generator.cpp
@@ -12,7 +12,13 @@ struct VertexPNC {
     // static const VertexDecl vDecl;
 };
 
-std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidCube(DevicePtr device) {
+enum class Axis : uint8_t {
+    X = 0,
+    Y = 1,
+    Z = 2,
+};
+
+std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidCube(DevicePtr& device) {
     uint32_t vertexCount = 24;
     VertexBufferBuilder vbBuilder;
     auto vb = vbBuilder.AddRange<VertexPNC>(vertexCount);
@@ -21,22 +27,27 @@ std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidCube(DevicePtr device) {
     vb[ 1].position	= dg::float3(-0.5f, 0.5f,-0.5f);
     vb[ 2].position	= dg::float3( 0.5f, 0.5f,-0.5f);
     vb[ 3].position	= dg::float3( 0.5f,-0.5f,-0.5f);
+
     vb[ 4].position	= dg::float3( 0.5f,-0.5f, 0.5f);
     vb[ 5].position	= dg::float3( 0.5f, 0.5f, 0.5f);
     vb[ 6].position	= dg::float3(-0.5f, 0.5f, 0.5f);
     vb[ 7].position	= dg::float3(-0.5f,-0.5f, 0.5f);
+
     vb[ 8].position	= dg::float3(-0.5f,-0.5f, 0.5f);
     vb[ 9].position	= dg::float3(-0.5f, 0.5f, 0.5f);
     vb[10].position	= dg::float3(-0.5f, 0.5f,-0.5f);
     vb[11].position	= dg::float3(-0.5f,-0.5f,-0.5f);
+
     vb[12].position	= dg::float3( 0.5f,-0.5f,-0.5f);
     vb[13].position	= dg::float3( 0.5f, 0.5f,-0.5f);
     vb[14].position	= dg::float3( 0.5f, 0.5f, 0.5f);
     vb[15].position	= dg::float3( 0.5f,-0.5f, 0.5f);
+
     vb[16].position	= dg::float3(-0.5f,-0.5f, 0.5f);
     vb[17].position	= dg::float3(-0.5f,-0.5f,-0.5f);
     vb[18].position	= dg::float3( 0.5f,-0.5f,-0.5f);
     vb[19].position	= dg::float3( 0.5f,-0.5f, 0.5f);
+
     vb[20].position	= dg::float3(-0.5f, 0.5f,-0.5f);
     vb[21].position	= dg::float3(-0.5f, 0.5f, 0.5f);
     vb[22].position	= dg::float3( 0.5f, 0.5f, 0.5f);
@@ -72,28 +83,29 @@ std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidCube(DevicePtr device) {
         ibBuilder.Build(device, "cube ib"), ib.OffsetBytes(), ib.Count(), ib.IsUint32());
 }
 
-std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidSphere(DevicePtr device, uint16_t cntVertexCircle) {
-    uint16_t plg = cntVertexCircle / 2 - 1;
+std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidSphere(DevicePtr& device, uint32_t cntCirclePoints) {
+    cntCirclePoints = std::max(cntCirclePoints, uint32_t(3));
+    uint32_t plg = cntCirclePoints / 2 - 1;
 
-    uint32_t vertexCount = static_cast<uint32_t>(plg) * static_cast<uint32_t>(cntVertexCircle) + uint32_t(2);
+    uint32_t vertexCount = plg * cntCirclePoints + uint32_t(2);
     VertexBufferBuilder vbBuilder;
     auto vb = vbBuilder.AddRange<VertexPNC>(vertexCount);
 
     float angleB = -HalfPI<float>();
     float stepB = PI<float>() / static_cast<float>(plg + 1);
-    float stepA = TwoPI<float>() / static_cast<float>(cntVertexCircle - 1);
+    float stepA = TwoPI<float>() / static_cast<float>(cntCirclePoints - 1);
 
     uint32_t ind = 1;
     for(auto ix=0; ix!=plg; ++ix) {
         angleB += stepB;
         float posY = std::sin(angleB) * 0.5f;
         float radius = std::cos(angleB) * 0.5f;
-        float textureV = 0.f - posY;
+        float textureV = 0.5 - posY;
 
         float angleA = 0.0f;
-        for(auto iy=0; iy!=cntVertexCircle; iy++) {
-            float posZ = std::sin(angleA) * radius;
+        for(auto iy=0; iy!=cntCirclePoints; iy++) {
             float posX = std::cos(angleA) * radius;
+            float posZ = std::sin(angleA) * radius;
             float textureU = angleA * 0.5f * OneOverPI<float>();
 
             vb[ind].position = dg::float3(posX, posY, posZ);
@@ -110,24 +122,24 @@ std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidSphere(DevicePtr device,
     vb[firstInd] = VertexPNC{dg::float3(0.0f,-0.5f,0.0f), dg::float3(0.0f,-1.0f,0.0f), dg::float2(0.5f,1.0f)};
     vb[lastInd]	 = VertexPNC{dg::float3(0.0f, 0.5f,0.0f), dg::float3(0.0f, 1.0f,0.0f), dg::float2(0.5f,0.0f)};
 
-    uint32_t indexCount = 6*(cntVertexCircle-1)*plg;
+    uint32_t indexCount = 6*(cntCirclePoints-1)*plg;
     IndexBufferBuilder ibBuilder;
     auto ib = ibBuilder.AddRange<uint32_t>(indexCount);
 
     ind=0;
     for(uint32_t ix=0; ix!=plg-1; ++ix) {
         uint32_t z1,z2,z3,z4;
-        z1 = ix*cntVertexCircle+1; z2 = z1+1;
-        z3 = z1+cntVertexCircle;   z4 = z2+cntVertexCircle;
-        for(auto iy=0; iy!=cntVertexCircle-1; iy++) {
+        z1 = ix*cntCirclePoints+1; z2 = z1+1;
+        z3 = z1+cntCirclePoints;   z4 = z2+cntCirclePoints;
+        for(auto iy=0; iy!=cntCirclePoints-1; iy++) {
             ib[ind++]=z1;	ib[ind++]=z3;	ib[ind++]=z4;
             ib[ind++]=z1;	ib[ind++]=z4;	ib[ind++]=z2;
             z1++; z2++; z3++; z4++;
         }
     }
 
-    uint32_t iy = cntVertexCircle*(plg-1);
-    for(uint32_t ix=1; ix!=cntVertexCircle; ix++) {
+    uint32_t iy = cntCirclePoints*(plg-1);
+    for(uint32_t ix=1; ix!=cntCirclePoints; ix++) {
         ib[ind++]=ix;       ib[ind++]=ix+1;   ib[ind++]=firstInd;
         ib[ind++]=iy+ix+1;  ib[ind++]=iy+ix;  ib[ind++]=lastInd;
     }
@@ -136,113 +148,100 @@ std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidSphere(DevicePtr device,
         ibBuilder.Build(device, "sphere ib"), ib.OffsetBytes(), ib.Count(), ib.IsUint32());
 }
 
-std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidCylinder(DevicePtr device, uint16_t cntVertexCircle) {
-	cntVertexCircle = std::max(cntVertexCircle, uint16_t(3));
+std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidCylinder(DevicePtr& device, uint32_t cntCirclePoints) {
+	cntCirclePoints = std::max(cntCirclePoints, uint32_t(3));
 
-    uint32_t vertexCount = 4 * cntVertexCircle;
+    uint32_t vertexCount = 2 * cntCirclePoints;
     VertexBufferBuilder vbBuilder;
     auto vb = vbBuilder.AddRange<VertexPNC>(vertexCount);
 
 	float angle = 0.f;
-	float step = TwoPI<float>() / static_cast<float>(cntVertexCircle);
-	for(uint32_t i=0; i!=cntVertexCircle; ++i) {
-        float s = std::sin(angle);
-        float c = std::cos(angle);
+	float step = TwoPI<float>() / static_cast<float>(cntCirclePoints - 1);
+	for(uint32_t i=0; i!=cntCirclePoints; ++i) {
+        float posX = std::cos(angle) * 0.5f;
+        float posZ = std::sin(angle) * 0.5f;
 		angle+=step;
 
-		dg::float3 pos(c*0.5f, -0.5f, s*0.5f);
-		dg::float3 norm = dg::normalize(dg::float3(pos.x, 0, pos.z));
-		dg::float2 tex(pos.x + 0.5f, pos.z + 0.5f);
+        dg::float3 normal = dg::normalize(dg::float3(posX, 0, posZ));
+        float textureU = static_cast<float>(i) / static_cast<float>(cntCirclePoints - 1);
 
-		float tu = static_cast<float>(i)/static_cast<float>(cntVertexCircle-1);
+		// bottom
+        uint32_t ind = i;
+		vb[ind].position = dg::float3(posX, -0.5f, posZ);
+		vb[ind].normal = normal;
+		vb[ind].uv = dg::float2(textureU, 1);
 
-		// bottom circle
-		vb[i].position	= pos;
-		vb[i].normal	= dg::float3(0,-1,0);
-		vb[i].uv	= tex;
-		// bottom side
-		vb[i+cntVertexCircle*1].position = pos;
-		vb[i+cntVertexCircle*1].normal   = norm;
-		vb[i+cntVertexCircle*1].uv       = dg::float2(tu,0);
-		// top side
-		pos.y = -pos.y;
-		vb[i+cntVertexCircle*2].position = pos;
-		vb[i+cntVertexCircle*2].normal   = norm;
-		vb[i+cntVertexCircle*2].uv       = dg::float2(tu,1);
-		// top circle
-		vb[i+cntVertexCircle*3].position = pos;
-		vb[i+cntVertexCircle*3].normal   = dg::float3(0, 1, 0);
-		vb[i+cntVertexCircle*3].uv       = tex;
+		// top
+        ind += cntCirclePoints;
+		vb[ind].position = dg::float3(posX, 0.5f, posZ);
+		vb[ind].normal = normal;
+		vb[ind].uv = dg::float2(textureU, 0);
 	}
 
-    uint32_t indexCount = (cntVertexCircle - 1) * 12;
+    uint32_t indexCount = (cntCirclePoints - 1) * 6;
     IndexBufferBuilder ibBuilder;
     auto ib = ibBuilder.AddRange<uint32_t>(indexCount);
 
-	uint32_t num = 0;
-	uint32_t addSm = (cntVertexCircle - 2)*3;
-	uint32_t addV = cntVertexCircle*3;
+    uint32_t ind = 0;
+    for(uint32_t i=0; i!=(cntCirclePoints - 1); ++i) {
+        uint32_t bottomLeftVertex = i;
+        uint32_t topLeftVertex = bottomLeftVertex + cntCirclePoints;
 
-    //  bottom + top circle
-	for(uint32_t i=2; i!=cntVertexCircle; ++i) {
-		ib[num+0] = 0;
-		ib[num+1] = i-1;
-		ib[num+2] = i;
+        ib[ind++] = bottomLeftVertex;
+        ib[ind++] = bottomLeftVertex + 1;
+        ib[ind++] = topLeftVertex;
 
-		ib[num+0+addSm] = addV;
-		ib[num+1+addSm] = addV+i-1;
-		ib[num+2+addSm] = addV+i;
-		num+=3;
-	}
-
-	// bottom + top side
-	num = addSm*2;
-	for(uint32_t i=0; i!=uint32_t(cntVertexCircle); ++i) {
-		uint32_t z1 = cntVertexCircle + i;
-		uint32_t z2 = cntVertexCircle + (i+1)%cntVertexCircle;
-		uint32_t z3 = cntVertexCircle + z1;
-		uint32_t z4 = cntVertexCircle + z2;
-		ib[num++] = z1;
-		ib[num++] = z3;
-		ib[num++] = z4;
-		ib[num++] = z1;
-		ib[num++] = z4;
-		ib[num++] = z2;
-	}
+        ib[ind++] = topLeftVertex;
+        ib[ind++] = bottomLeftVertex + 1;
+        ib[ind++] = topLeftVertex + 1;
+    }
 
     return std::make_shared<GeometryNode>(vbBuilder.Build(device, "cylinder vb"), vb.OffsetBytes(),
         ibBuilder.Build(device, "cylinder ib"), ib.OffsetBytes(), ib.Count(), ib.IsUint32());
 }
 
-std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidPlaneXZ(DevicePtr device, uint32_t cntXPoints, uint32_t cntZPoints, float scaleTextureX, float scaleTextureZ) {
-    cntXPoints = std::max(cntXPoints, uint32_t(2));
-    cntZPoints = std::max(cntZPoints, uint32_t(2));
+static std::shared_ptr<GeometryNode> CreateSolidPlane(DevicePtr& device, uint32_t cntWidthPoints, uint32_t cntHeightPoints, float scaleTextureWidth, float scaleTextureHeight, Axis axisUp) {
+    cntWidthPoints = std::max(cntWidthPoints, uint32_t(2));
+    cntHeightPoints = std::max(cntHeightPoints, uint32_t(2));
 
-    uint32_t vertexCount = cntXPoints * cntZPoints;
+    uint32_t vertexCount = cntWidthPoints * cntHeightPoints;
     VertexBufferBuilder vbBuilder;
     auto vb = vbBuilder.AddRange<VertexPNC>(vertexCount);
 
     uint32_t ind = 0;
-    for(uint32_t i=0; i!=cntXPoints; ++i) {
-        float tu = static_cast<float>(i) / static_cast<float>(cntXPoints - 1);
-        for(uint32_t j=0; j!=cntZPoints; ++j) {
-            float tv = static_cast<float>(j) / static_cast<float>(cntZPoints - 1);
-            vb[ind].position = dg::float3(tu - 0.5f, 0.0f, tv - 0.5f);
-            vb[ind].normal = dg::float3(0.0f, 1.0f, 0.0f);
-            vb[ind].uv = dg::float2(scaleTextureX * tu, scaleTextureZ * tv);
+    for(uint32_t i=0; i!=cntWidthPoints; ++i) {
+        float tu = static_cast<float>(i) / static_cast<float>(cntWidthPoints - 1);
+        for(uint32_t j=0; j!=cntHeightPoints; ++j) {
+            float tv = 1.f - static_cast<float>(j) / static_cast<float>(cntHeightPoints - 1);
+            switch (axisUp) {
+                case Axis::X:
+                    vb[ind].position = dg::float3(0.f, 0.5f - tv, tu - 0.5f);
+                    vb[ind].normal = dg::float3(1.f, 0.f, 0.f);
+                    break;
+                case Axis::Y:
+                    vb[ind].position = dg::float3(tu - 0.5f, 0.f, 0.5f - tv);
+                    vb[ind].normal = dg::float3(0.f, 1.f, 0.f);
+                    break;
+                case Axis::Z:
+                default:
+                    vb[ind].position = dg::float3(tu - 0.5f, 0.5f - tv, 0.f);
+                    vb[ind].normal = dg::float3(0.f, 0.f, 1.f);
+                    break;
+            }
+            vb[ind].uv = dg::float2(scaleTextureWidth * tu, scaleTextureHeight * tv);
             ++ind;
         }
     }
 
-    uint32_t indexCount = (cntXPoints - 1) * (cntZPoints - 1) * 6;
+    uint32_t indexCount = (cntWidthPoints - 1) * (cntHeightPoints - 1) * 6;
     IndexBufferBuilder ibBuilder;
     auto ib = ibBuilder.AddRange<uint32_t>(indexCount);
 
     ind = 0;
-    for(uint32_t i=0; i!=(cntXPoints - 1); ++i) {
-        for(uint32_t j=0; j!=(cntZPoints - 1); ++j) {
-            uint32_t bottomLeftVertex = i * cntXPoints + j;
-            uint32_t topLeftVertex = bottomLeftVertex + cntZPoints;
+    for(uint32_t i=0; i!=(cntWidthPoints - 1); ++i) {
+        for(uint32_t j=0; j!=(cntHeightPoints - 1); ++j) {
+            uint32_t bottomLeftVertex = i * cntWidthPoints + j;
+            uint32_t topLeftVertex = bottomLeftVertex + cntHeightPoints;
 
             ib[ind++] = bottomLeftVertex;
             ib[ind++] = bottomLeftVertex + 1;
@@ -256,4 +255,16 @@ std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidPlaneXZ(DevicePtr device
 
     return std::make_shared<GeometryNode>(vbBuilder.Build(device, "plane vb"), vb.OffsetBytes(),
         ibBuilder.Build(device, "plane ib"), ib.OffsetBytes(), ib.Count(), ib.IsUint32());
+}
+
+std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidPlaneXZ(DevicePtr& device, uint32_t cntXPoints, uint32_t cntZPoints, float scaleTextureX, float scaleTextureZ) {
+    return CreateSolidPlane(device, cntXPoints, cntZPoints, scaleTextureX, scaleTextureZ, Axis::Y);
+}
+
+std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidPlaneXY(DevicePtr& device, uint32_t cntXPoints, uint32_t cntYPoints, float scaleTextureX, float scaleTextureY) {
+    return CreateSolidPlane(device, cntXPoints, cntYPoints, scaleTextureX, scaleTextureY, Axis::Z);
+}
+
+std::shared_ptr<GeometryNode> MeshGenerator::CreateSolidPlaneZY(DevicePtr& device, uint32_t cntZPoints, uint32_t cntYPoints, float scaleTextureZ, float scaleTextureY) {
+    return CreateSolidPlane(device, cntZPoints, cntYPoints, scaleTextureZ, scaleTextureY, Axis::X);
 }
--- src/middleware/generator/mesh_generator.h
+++ src/middleware/generator/mesh_generator.h
@@ -26,23 +26,39 @@ struct MeshGenerator {
             /       /
           17------18
     */
-    static std::shared_ptr<GeometryNode> CreateSolidCube(DevicePtr device);
+    static std::shared_ptr<GeometryNode> CreateSolidCube(DevicePtr& device);
     /*!
         Creates a sphere with a center at the beginning of coordinates and a diameter equal to 1
-        cntVertexCircle - Number of vertices in the circle
+        cntCirclePoints - Number of vertices in one circle of sphere
     */
-    static std::shared_ptr<GeometryNode> CreateSolidSphere(DevicePtr device, uint16_t cntVertexCircle);
+    static std::shared_ptr<GeometryNode> CreateSolidSphere(DevicePtr& device, uint32_t cntCirclePoints);
     /*!
         Creates a cylinder with a center at the beginning of coordinates, with a diameter and height equal to 1
-        cntVertexCircle - Number of vertices in the base circle
+        cntCirclePoints - Number of vertices in the base circle
     */
-    static std::shared_ptr<GeometryNode> CreateSolidCylinder(DevicePtr device, uint16_t cntVertexCircle);
+    static std::shared_ptr<GeometryNode> CreateSolidCylinder(DevicePtr& device, uint32_t cntCirclePoints);
     /*!
-        Creates a square plane at the beginning of the coordinates with the edge side equal to 1.
+        Creates a square plane with a center at the beginning of coordinates with the edge side equal to 1.
         The plane is located in the X0Z plane, the normals are directed along the Y axis.
 
-        cntXSides - Number of vertices on 0X axis (cntXSides >= 2)
-        cntZSides - Number of vertices on 0Z axis (cntZSides >= 2)
+        cntXPoints - Number of vertices on 0X axis (cntXPoints >= 2)
+        cntZPoints - Number of vertices on 0Z axis (cntZPoints >= 2)
     */
-    static std::shared_ptr<GeometryNode> CreateSolidPlaneXZ(DevicePtr device, uint32_t cntXPoints = 2, uint32_t cntZPoints = 2, float scaleTextureX = 1.0f, float scaleTextureZ = 1.0f);
+    static std::shared_ptr<GeometryNode> CreateSolidPlaneXZ(DevicePtr& device, uint32_t cntXPoints = 2, uint32_t cntZPoints = 2, float scaleTextureX = 1.f, float scaleTextureZ = 1.f);
+    /*!
+        Creates a square plane with a center at the beginning of coordinates with the edge side equal to 1.
+        The plane is located in the X0Y plane, the normals are directed along the Z axis.
+
+        cntXPoints - Number of vertices on 0X axis (cntXPoints >= 2)
+        cntYPoints - Number of vertices on 0Y axis (cntYPoints >= 2)
+    */
+    static std::shared_ptr<GeometryNode> CreateSolidPlaneXY(DevicePtr& device, uint32_t cntXPoints = 2, uint32_t cntYPoints = 2, float scaleTextureX = 1.f, float scaleTextureY = 1.f);
+    /*!
+        Creates a square plane with a center at the beginning of coordinates with the edge side equal to 1.
+        The plane is located in the Z0Y plane, the normals are directed along the X axis.
+
+        cntZPoints - Number of vertices on 0Z axis (cntZPoints >= 2)
+        cntYPoints - Number of vertices on 0Y axis (cntYPoints >= 2)
+    */
+    static std::shared_ptr<GeometryNode> CreateSolidPlaneZY(DevicePtr& device, uint32_t cntZPoints = 2, uint32_t cntYPoints = 2, float scaleTextureZ = 1.f, float scaleTextureY = 1.f);
 };
