4807bc4f97b66eb25ddcbbd34125021a56683604
ReanGD <reangd@yandex.ru>
2020-01-28 00:50:17 +0300

    update to new virsion of Diligent

--- third_party/DiligentGraphics/conanfile.py
+++ third_party/DiligentGraphics/conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 
 class DiligentGraphics(ConanFile):
     name = "DiligentGraphics"
-    version = "2.4.7685592.1ad5f15.2"
+    version = "2.4.7795c56.b3f3da2"
     license = "Apache License 2.0"
     url = "https://github.com/DiligentGraphics/DiligentGraphics"
     description = " A modern cross-platform low-level graphics library and rendering framework"
@@ -19,36 +19,24 @@ class DiligentGraphics(ConanFile):
         "development": False,
     }
     generators = "cmake"
+    exports_sources = "DiligentGraphics/*"
 
     def source(self):
-        os.mkdir("DiligentGraphics")
-
-        core_version = "7685592"
+        core_version = "7795c56"
         self.run("git clone --recursive https://github.com/DiligentGraphics/DiligentCore.git DiligentGraphics/DiligentCore")
         with tools.chdir("DiligentGraphics/DiligentCore"):
             self.run("git reset --hard %s" % core_version)
 
-        tools_version = "1ad5f15"
+        tools_version = "b3f3da2"
         self.run("git clone --recursive https://github.com/DiligentGraphics/DiligentTools.git DiligentGraphics/DiligentTools")
         with tools.chdir("DiligentGraphics/DiligentTools"):
             self.run("git reset --hard %s" % tools_version)
-
-        with tools.chdir("DiligentGraphics"):
-            if not os.path.exists("CMakeLists.txt"):
-                f=open("CMakeLists.txt","w")
-                f.write("cmake_minimum_required (VERSION 3.6)\n")
-                f.write("set_property(GLOBAL PROPERTY USE_FOLDERS ON)\n")
-                f.write("project(DiligentGraphics)\n")
-                f.write("add_subdirectory(DiligentCore)\n")
-                f.write("add_subdirectory(DiligentTools)\n")
-                f.close()
 
     def _create_cmake(self):
         cmake = CMake(self)
         if self.options.development:
             cmake.definitions["DEVELOPMENT"] = 1
         cmake.configure(source_folder="DiligentGraphics")
-        cmake.build()
         return cmake
 
     def build(self):
@@ -59,15 +47,7 @@ class DiligentGraphics(ConanFile):
         cmake = self._create_cmake()
         cmake.install()
 
-        self.copy("libDiligentCore.a", src="DiligentCore", dst="lib", keep_path=False)
-        self.copy("*.a", src="DiligentCore/Common", dst="lib", keep_path=False)
-        self.copy("*.a", src="DiligentCore/Graphics/GraphicsTools", dst="lib", keep_path=False)
-        self.copy("*.so", src="DiligentCore/Graphics/GraphicsEngineVulkan", dst="lib", keep_path=False)
-        self.copy("*.so", src="DiligentCore/Graphics/GraphicsEngineOpenGL", dst="lib", keep_path=False)
-        self.copy("*.a", src="DiligentTools/TextureLoader", dst="lib", keep_path=False)
-        self.copy("*.a", src="DiligentTools/Imgui", dst="lib", keep_path=False)
-        self.copy("*.a", src="DiligentTools/ThirdParty", dst="lib", keep_path=False)
-
     def package_info(self):
-        self.cpp_info.includedirs = ['headers']
+        build_type = str(self.settings.build_type)
+        self.cpp_info.libdirs = [os.path.join('lib', 'DiligentCore', build_type), os.path.join('lib', 'DiligentTools', build_type)]
         self.cpp_info.libs = tools.collect_libs(self)
