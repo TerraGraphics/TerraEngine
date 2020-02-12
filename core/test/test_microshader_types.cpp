c7671c1502f449881d856f2caba91f7a9c413cbc
ReanGD <reangd@yandex.ru>
2020-02-13 01:23:47 +0300

    add gtest

---
+++ core/test/test_microshader_types.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+#include "core/material/microshader_types.h"
+
+namespace {
+using namespace msh;
+
+TEST(Items, Generate) {
+    std::vector<Item> data = {"a", "b", "c"};
+    Items items;
+    items.SetData(std::move(data));
+
+    std::string out;
+    items.Generate([](const Item& item, std::string& out){ out.append(item + ","); }, out);
+
+    ASSERT_EQ("a,b,c,", out);
+}
+
+}
