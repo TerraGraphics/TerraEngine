7ef2736c65ce3b173bf545cd6dc9341361bbe418
ReanGD <reangd@yandex.ru>
2020-02-16 00:13:12 +0300

    add cloc script

---
+++ Makefile
@@ -0,0 +1,7 @@
+.PHONY: cloc help
+
+cloc: ## Count line of code
+	@./ci/make_scripts/cloc.sh
+
+help: ## Display this help screen
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
---
+++ ci/make_scripts/cloc.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+for directory in platforms core middleware editor ""
+do	
+	number=$(scc --ci --no-cocomo --no-complexity --include-ext=cpp,c,h,hpp ./$directory | grep Total | awk '{print $NF}')
+	name=$directory
+	if test -z "$directory"
+	then
+		name="TOTAL"
+	fi
+
+	printf "%10s: %d\n" $name $number
+done
