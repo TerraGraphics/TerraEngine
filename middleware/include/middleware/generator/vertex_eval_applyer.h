0a81a39f93379f2c9049a0210588aa559a171234
ReanGD <reangd@yandex.ru>
2020-03-06 21:00:54 +0300

    add VertexEvalApplyer

---
+++ middleware/include/middleware/generator/vertex_eval_applyer.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <functional>
+
+#include "core/common/ctor.h"
+#include "middleware/generator/shape_generator.h"
+
+
+template<class TShape>
+    class VertexEvalApplyer : public IShapeGenerator, Fixed {
+public:
+    using Callback = std::function<void (VertexPNC* /* begin */, VertexPNC* /* end */)>;
+    VertexEvalApplyer(TShape baseGenerator, Callback&& callback)
+        : m_baseGenerator(std::move(baseGenerator))
+        , m_callback(std::move(callback)) {
+
+    }
+
+    size_t LenghtVertex() const final {
+        return m_baseGenerator.LenghtVertex();
+    }
+
+    size_t LenghtIndex() const final {
+        return m_baseGenerator.LenghtIndex();
+    }
+
+    void FillVertex(VertexPNC* vertexes) const final {
+        m_baseGenerator.FillVertex(vertexes);
+        m_callback(vertexes, vertexes + m_baseGenerator.LenghtVertex());
+    }
+
+    void FillIndex(uint32_t* indexes, uint32_t vertexStartIndex) const final {
+        m_baseGenerator.FillIndex(indexes, vertexStartIndex);
+    }
+
+private:
+    TShape m_baseGenerator;
+    Callback m_callback;
+};
