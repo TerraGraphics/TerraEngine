9c5930e6e287f230440a4c62291a732ae97c4f44
ReanGD <reangd@yandex.ru>
2020-03-13 02:26:39 +0300

    tests for cubic equations

--- core/test/test_math_equation.cpp
+++ core/test/test_math_equation.cpp
@@ -6,22 +6,17 @@
 #include "core/math/equation.h"
 
 
-#define ASSERT_FLOAT3_EQ(actual, expected) \
-    ASSERT_FLOAT_EQ(actual.x, expected.x); \
-    ASSERT_FLOAT_EQ(actual.y, expected.y); \
-    ASSERT_FLOAT_EQ(actual.z, expected.z);
+#define EXPECT_QUAD(a, b, c, result) \
+    ASSERT_DOUBLE_EQ(a * result * result + b * result + c, 0);
 
-#define ASSERT_FLOAT4_EQ(actual, expected) \
-    ASSERT_FLOAT_EQ(actual.x, expected.x); \
-    ASSERT_FLOAT_EQ(actual.y, expected.y); \
-    ASSERT_FLOAT_EQ(actual.z, expected.z); \
-    ASSERT_FLOAT_EQ(actual.w, expected.w);
+#define EXPECT_QUAD_FLOAT(a, b, c, result) \
+    ASSERT_FLOAT_EQ(a * result * result + b * result + c, 0);
 
-#define EXPECT_FLOAT4_NEAR(actual, expected, abs_error) \
-    EXPECT_NEAR(actual.x, expected.x, abs_error); \
-    EXPECT_NEAR(actual.y, expected.y, abs_error); \
-    EXPECT_NEAR(actual.z, expected.z, abs_error); \
-    EXPECT_NEAR(actual.w, expected.w, abs_error);
+#define EXPECT_CUBIC(a, b, c, d, result) \
+    EXPECT_NEAR(a * result * result * result + b * result * result + c * result + d, 0, 1.e-13);
+
+#define EXPECT_CUBIC_FLOAT(a, b, c, d, result) \
+    EXPECT_NEAR(a * result * result * result + b * result * result + c * result + d, 0, 1.e-5);
 
 namespace {
 
@@ -86,15 +81,15 @@ TEST(MathEquation, SolveQuadDouble) {
     b = 6.;
     c = 9.;
     ASSERT_EQ(math::SolveQuad(a, b, c, result), 2); // -3, -3
-    ASSERT_DOUBLE_EQ(a * result[0] * result[0] + b * result[0] + c, 0);
-    ASSERT_DOUBLE_EQ(a * result[1] * result[1] + b * result[1] + c, 0);
+    EXPECT_QUAD(a, b, c, result[0]);
+    EXPECT_QUAD(a, b, c, result[1]);
 
     a = 1.;
     b = 2.;
     c = -3.;
     ASSERT_EQ(math::SolveQuad(a, b, c, result), 2); // 1, -3
-    ASSERT_DOUBLE_EQ(a * result[0] * result[0] + b * result[0] + c, 0);
-    ASSERT_DOUBLE_EQ(a * result[1] * result[1] + b * result[1] + c, 0);
+    EXPECT_QUAD(a, b, c, result[0]);
+    EXPECT_QUAD(a, b, c, result[1]);
 }
 
 TEST(MathEquation, SolveQuadDoubleAIsZero) {
@@ -105,7 +100,7 @@ TEST(MathEquation, SolveQuadDoubleAIsZero) {
     b = 2.;
     c = 3.;
     ASSERT_EQ(math::SolveQuad(a, b, c, result), 1); // -2
-    ASSERT_DOUBLE_EQ(a * result[0] * result[0] + b * result[0] + c, 0);
+    EXPECT_QUAD(a, b, c, result[0]);
 }
 
 TEST(MathEquation, SolveQuadDoubleAIsZeroBIsZero) {
@@ -131,15 +126,15 @@ TEST(MathEquation, SolveQuadFloat) {
     b = 6.f;
     c = 9.f;
     ASSERT_EQ(math::SolveQuad(a, b, c, result), 2); // -3, -3
-    ASSERT_FLOAT_EQ(a * result[0] * result[0] + b * result[0] + c, 0);
-    ASSERT_FLOAT_EQ(a * result[1] * result[1] + b * result[1] + c, 0);
+    EXPECT_QUAD_FLOAT(a, b, c, result[0]);
+    EXPECT_QUAD_FLOAT(a, b, c, result[1]);
 
     a = 1.f;
     b = 2.f;
     c = -3.f;
     ASSERT_EQ(math::SolveQuad(a, b, c, result), 2); // 1, -3
-    ASSERT_FLOAT_EQ(a * result[0] * result[0] + b * result[0] + c, 0);
-    ASSERT_FLOAT_EQ(a * result[1] * result[1] + b * result[1] + c, 0);
+    EXPECT_QUAD_FLOAT(a, b, c, result[0]);
+    EXPECT_QUAD_FLOAT(a, b, c, result[1]);
 }
 
 TEST(MathEquation, SolveQuadFloatAIsZero) {
@@ -150,7 +145,7 @@ TEST(MathEquation, SolveQuadFloatAIsZero) {
     b = 2.f;
     c = 3.f;
     ASSERT_EQ(math::SolveQuad(a, b, c, result), 1); // -2
-    ASSERT_FLOAT_EQ(a * result[0] * result[0] + b * result[0] + c, 0);
+    EXPECT_QUAD_FLOAT(a, b, c, result[0]);
 }
 
 TEST(MathEquation, SolveQuadFloatAIsZeroBIsZero) {
@@ -161,6 +156,88 @@ TEST(MathEquation, SolveQuadFloatAIsZeroBIsZero) {
     b = 0.f;
     c = 3.f;
     ASSERT_EQ(math::SolveQuad(a, b, c, result), 0);
+}
+
+TEST(MathEquation, SolveCubicDouble) {
+    double a, b, c, d;
+    double result[3];
+
+    a = 1.;
+    b = 2.;
+    c = 3.;
+    d = 4.;
+    ASSERT_EQ(math::SolveCubic(a, b, c, d, result), 1); // - 1.6506
+    EXPECT_CUBIC(a, b, c, d, result[0]);
+
+    a = 1.;
+    b = 2.;
+    c = 3.;
+    d = -1.;
+    ASSERT_EQ(math::SolveCubic(a, b, c, d, result), 1); // - 0.27568
+    EXPECT_CUBIC(a, b, c, d, result[0]);
+
+    a = 5.;
+    b = -8.;
+    c = -8.;
+    d = 5.;
+    ASSERT_EQ(math::SolveCubic(a, b, c, d, result), 3);
+    EXPECT_CUBIC(a, b, c, d, result[0]);
+    EXPECT_CUBIC(a, b, c, d, result[1]);
+    EXPECT_CUBIC(a, b, c, d, result[2]);
+}
+
+TEST(MathEquation, SolveCubicDoubleAIsZero) {
+    double a, b, c, d;
+    double result[3];
+
+    a = 0.;
+    b = 1.;
+    c = 6.;
+    d = 9.;
+    ASSERT_EQ(math::SolveCubic(a, b, c, d, result), 2);
+    EXPECT_CUBIC(a, b, c, d, result[0]);
+    EXPECT_CUBIC(a, b, c, d, result[1]);
+}
+
+TEST(MathEquation, SolveCubicFloat) {
+    float a, b, c, d;
+    float result[3];
+
+    a = 1.f;
+    b = 2.f;
+    c = 3.f;
+    d = 4.f;
+    ASSERT_EQ(math::SolveCubic(a, b, c, d, result), 1); // - 1.6506
+    EXPECT_CUBIC_FLOAT(a, b, c, d, result[0]);
+
+    a = 1.f;
+    b = 2.f;
+    c = 3.f;
+    d = -1.f;
+    ASSERT_EQ(math::SolveCubic(a, b, c, d, result), 1); // - 0.27568
+    EXPECT_CUBIC_FLOAT(a, b, c, d, result[0]);
+
+    a = 5.f;
+    b = -8.f;
+    c = -8.f;
+    d = 5.f;
+    ASSERT_EQ(math::SolveCubic(a, b, c, d, result), 3);
+    EXPECT_CUBIC_FLOAT(a, b, c, d, result[0]);
+    EXPECT_CUBIC_FLOAT(a, b, c, d, result[1]);
+    EXPECT_CUBIC_FLOAT(a, b, c, d, result[2]);
+}
+
+TEST(MathEquation, SolveCubicFloatAIsZero) {
+    float a, b, c, d;
+    float result[3];
+
+    a = 0.f;
+    b = 1.f;
+    c = 6.f;
+    d = 9.f;
+    ASSERT_EQ(math::SolveCubic(a, b, c, d, result), 2);
+    EXPECT_CUBIC_FLOAT(a, b, c, d, result[0]);
+    EXPECT_CUBIC_FLOAT(a, b, c, d, result[1]);
 }
 
 }
