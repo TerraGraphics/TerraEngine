a76dd7a5e231776cc91d5b2687dc8b77fa33ae51
ReanGD <reangd@yandex.ru>
2020-03-15 02:03:32 +0300

    add tests for quartic

--- core/test/test_math_equation.cpp
+++ core/test/test_math_equation.cpp
@@ -51,17 +51,24 @@ template<typename T> static T Cubic(T a, T b, T c, T d, T result) {
 #define EXPECT_FULL_CUBIC(a, b, c, d, result) \
     EXPECT_NEAR(Cubic(a, b, c, d, result), 0, CubicAbsError<decltype(a)>());
 
+template<typename T> static T QuarticAbsError() {
+    if constexpr (std::is_same<T, double>::value) {
+        return 1.e-12;
+    }
+
+    return 1.e-5f;
 }
 
-#define EXPECT_CUBIC(a, b, c, result) \
-    EXPECT_NEAR(Cubic(static_cast<decltype(a)>(1.), a, b, c, result), 0, CubicAbsError<decltype(a)>());
+template<typename T> static T Quartic(T a, T b, T c, T d, T e, T result) {
+    double r = static_cast<double>(result);
+    return static_cast<T>((((static_cast<double>(a) * r + static_cast<double>(b)) * r + static_cast<double>(c)) * r + static_cast<double>(d)) * r + static_cast<double>(e));
+}
 
-#define EXPECT_FULL_CUBIC(a, b, c, d, result) \
-    EXPECT_NEAR(Cubic(a, b, c, d, result), 0, CubicAbsError<decltype(a)>());
+#define EXPECT_QUARTIC(a, b, c, d, result) \
+    EXPECT_NEAR(Quartic(static_cast<decltype(a)>(1.), a, b, c, d, result), 0, QuarticAbsError<decltype(a)>());
 
-TEST(MathEquation, SolveLinearDouble) {
-    double a, b;
-    double result[1];
+#define EXPECT_FULL_QUARTIC(a, b, c, d, e, result) \
+    EXPECT_NEAR(Quartic(a, b, c, d, e, result), 0, QuarticAbsError<decltype(a)>());
 
 namespace {
 
@@ -389,47 +396,93 @@ TYPED_TEST(MathEquation, SolveFullCubicAIsZero) {
     EXPECT_FULL_CUBIC(a, b, c, d, result[1]);
 }
 
-TEST(MathEquation, SolveCubicFloat) {
-    float a, b, c, d;
-    float result[3];
+TYPED_TEST(MathEquation, SolveQuartic) {
+    TypeParam a, b, c, d;
+    TypeParam result[4];
 
-    a = 1.f;
-    b = 2.f;
-    c = 3.f;
-    d = 4.f;
-    ASSERT_EQ(math::SolveCubic(a, b, c, d, result), 1); // - 1.6506
-    EXPECT_FULL_CUBIC(a, b, c, d, result[0]);
+    a = 1;
+    b = 2;
+    c = 3;
+    d = 4;
+    ASSERT_EQ(math::SolveQuartic(a, b, c, d, result), 0);
 
-    a = 1.f;
-    b = 2.f;
-    c = 3.f;
-    d = -1.f;
-    ASSERT_EQ(math::SolveCubic(a, b, c, d, result), 1); // - 0.27568
-    EXPECT_FULL_CUBIC(a, b, c, d, result[0]);
+    a = 3;
+    b = 3;
+    c = -1;
+    d = -6;
+    ASSERT_EQ(math::SolveQuartic(a, b, c, d, result), 2);
+    EXPECT_QUARTIC(a, b, c, d, result[0]);
+    EXPECT_QUARTIC(a, b, c, d, result[1]);
 
-    a = 5.f;
-    b = -8.f;
-    c = -8.f;
-    d = 5.f;
-    ASSERT_EQ(math::SolveCubic(a, b, c, d, result), 3);
-    EXPECT_FULL_CUBIC(a, b, c, d, result[0]);
-    EXPECT_FULL_CUBIC(a, b, c, d, result[1]);
-    EXPECT_FULL_CUBIC(a, b, c, d, result[2]);
+    a = -3;
+    b = static_cast<TypeParam>(-4. / 9.);
+    c = 3;
+    d = 1;
+    ASSERT_EQ(math::SolveQuartic(a, b, c, d, result), 4);
+    EXPECT_QUARTIC(a, b, c, d, result[0]);
+    EXPECT_QUARTIC(a, b, c, d, result[1]);
+    EXPECT_QUARTIC(a, b, c, d, result[2]);
+    EXPECT_QUARTIC(a, b, c, d, result[3]);
 }
 
-TEST(MathEquation, SolveCubicFloatAIsZero) {
-    float a, b, c, d;
-    float result[3];
+TYPED_TEST(MathEquation, SolveQuarticAIsZero) {
+    TypeParam a, b, c, d;
+    TypeParam result[4];
 
-    a = 0.f;
-    b = 1.f;
-    c = 6.f;
-    d = 9.f;
-    ASSERT_EQ(math::SolveCubic(a, b, c, d, result), 2);
-    EXPECT_FULL_CUBIC(a, b, c, d, result[0]);
-    EXPECT_FULL_CUBIC(a, b, c, d, result[1]);
+    a = 0;
+    b = -1;
+    c = 2;
+    d = -1;
+    ASSERT_EQ(math::SolveQuartic(a, b, c, d, result), 2);
+    EXPECT_QUARTIC(a, b, c, d, result[0]);
+    EXPECT_QUARTIC(a, b, c, d, result[1]);
 }
 
+TYPED_TEST(MathEquation, SolveFullQuartic) {
+    TypeParam a, b, c, d, e;
+    TypeParam result[4];
+
+    a = 2;
+    b = 2;
+    c = 3;
+    d = 6;
+    e = 8;
+    ASSERT_EQ(math::SolveQuartic(a, b, c, d, e, result), 0);
+
+    a = 2;
+    b = 6;
+    c = 6;
+    d = -2;
+    e = -12;
+    ASSERT_EQ(math::SolveQuartic(a, b, c, d, e, result), 2);
+    EXPECT_FULL_QUARTIC(a, b, c, d, e, result[0]);
+    EXPECT_FULL_QUARTIC(a, b, c, d, e, result[1]);
+
+    a = 6;
+    b = -35;
+    c = 62;
+    d = -35;
+    e = 6;
+    ASSERT_EQ(math::SolveQuartic(a, b, c, d, e, result), 4);
+    EXPECT_FULL_QUARTIC(a, b, c, d, e, result[0]);
+    EXPECT_FULL_QUARTIC(a, b, c, d, e, result[1]);
+    EXPECT_FULL_QUARTIC(a, b, c, d, e, result[2]);
+    EXPECT_FULL_QUARTIC(a, b, c, d, e, result[3]);
+}
+
+TYPED_TEST(MathEquation, SolveFullQuarticAIsZero) {
+    TypeParam a, b, c, d, e;
+    TypeParam result[4];
+
+    a = 0;
+    b = 2;
+    c = -11;
+    d = 12;
+    e = 9;
+    ASSERT_EQ(math::SolveQuartic(a, b, c, d, e, result), 3);
+    EXPECT_FULL_QUARTIC(a, b, c, d, e, result[0]);
+    EXPECT_FULL_QUARTIC(a, b, c, d, e, result[1]);
+    EXPECT_FULL_QUARTIC(a, b, c, d, e, result[2]);
 }
 
 }
