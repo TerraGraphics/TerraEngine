12cd2f21875bf6a0251fdc0c0118187d3fb807f0
ReanGD <reangd@yandex.ru>
2020-05-02 22:40:08 +0300

    add fast pimpl

--- core/include/core/common/ctor.h
+++ core/include/core/common/ctor.h
@@ -1,5 +1,6 @@
 #pragma once
 
+
 class Noncopyable {
 protected:
     Noncopyable() = default;
---
+++ core/include/core/common/pimpl.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <new>
+#include <utility>
+// #include <type_traits>
+
+
+template<class T, std::size_t Size, std::size_t Alignment> class Pimpl {
+public:
+    template<class...Args> explicit Pimpl(Args&&... args) {
+        new (RawPtr()) T(std::forward<Args>(args)...);
+    }
+
+    Pimpl(const Pimpl& other) {
+        new (RawPtr()) T(*other);
+    }
+
+    Pimpl(Pimpl&& other) noexcept = default;
+
+    ~Pimpl() noexcept {
+        Validate<sizeof(T), alignof(T)>();
+        RawPtr()->~T();
+    }
+
+    Pimpl& operator=(Pimpl&& rhs) noexcept {
+        *RawPtr() = std::move(*rhs);
+        return *this;
+    }
+
+    T* RawPtr() noexcept {return reinterpret_cast<T*>(&m_data);}
+    const T* RawPtr() const noexcept {return reinterpret_cast<const T*>(&m_data);}
+
+    T* operator->() noexcept { return RawPtr(); }
+    const T* operator->() const noexcept { return RawPtr(); }
+
+    T& operator*() noexcept { return *RawPtr(); }
+    const T& operator*() const noexcept { return *RawPtr(); }
+
+private:
+    template<std::size_t ActualSize, std::size_t ActualAlignment>
+        static void Validate() noexcept {
+            static_assert(Size == ActualSize, "Size and sizeof(T) mismatch");
+            static_assert(Alignment == ActualAlignment,"Alignment and alignof(T) mismatch");
+        }
+
+private:
+    std::aligned_storage_t<Size, Alignment> m_data;
+};
