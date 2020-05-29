9aa15772fa2d33958ac6fa78721a824a4075464b
ReanGD <reangd@yandex.ru>
2020-05-30 02:55:13 +0300

    implement add node

--- middleware/include/middleware/gscheme/graph/gs_graph.h
+++ middleware/include/middleware/gscheme/graph/gs_graph.h
@@ -4,7 +4,7 @@
 
 /*
     valid ID (pinID, linkID, nodeID) more than 0
-    uint16_t nodeID
+    uint16_t nodeID - index in Graph::m_nodes + 1
     uint32_t pinID = nodeID << 16 | pinNumber << 8 | input / output, embedded or not
     uint64_t linkID = outputPinID << 32 | inputPinID
 */
@@ -22,21 +22,52 @@ struct Pin {
     void* data = nullptr;
 };
 
-struct Node {
-    uint16_t id = 0;
-    uint8_t countInputPins = 0;
-    uint8_t countOutputPins = 0;
-    Pin* pins = nullptr;
-    void* data = nullptr;
+class Node {
+public:
+    Node() = default;
+    ~Node() = default;
+
+    void Init(uint16_t id);
+    void Clear(uint16_t nextFreeIndex);
+
+    // return nextFreeIndex
+    uint16_t Create(uint8_t countInputPins, uint8_t countOutputPins, void* data);
+
+private:
+    // nodeID - index in Graph::m_nodes + 1
+    uint16_t m_id = 0;
+
+#pragma GCC diagnostic push
+#if defined(__clang__)
+#pragma clang diagnostic ignored "-Wnested-anon-types"
+#pragma clang diagnostic ignored "-Wgnu-anonymous-struct"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
+    union {
+        // index in Graph::m_nodes
+        uint16_t m_nextFreeIndex = 0;
+        struct {
+            uint8_t m_countInputPins;
+            uint8_t m_countOutputPins;
+        };
+    };
+#pragma GCC diagnostic pop
+
+    Pin* m_pins = nullptr;
+    void* m_data = nullptr;
 };
 
 class Graph {
     Graph(uint16_t initialNodeCount = 16);
     ~Graph();
 
+    Node& AddNode(uint8_t countInputPins, uint8_t countOutputPins, void* data);
+
 private:
     uint16_t m_free = 0;
     uint16_t m_capacity = 0;
+    uint16_t m_firstFree = 0;
     Node* m_nodes = nullptr;
 };
 
--- middleware/src/gscheme/graph/gs_graph.cpp
+++ middleware/src/gscheme/graph/gs_graph.cpp
@@ -1,17 +1,92 @@
 #include "middleware/gscheme/graph/gs_graph.h"
 
+#include <limits>
+#include <cstring>
+
+#include "core/common/exception.h"
+
+
 namespace gs {
+
+static_assert(sizeof(Pin) == 16, "sizeof(Pin) == 16 bytes");
+static_assert(sizeof(Node) == 24, "sizeof(Node) == 24 bytes");
+static_assert(sizeof(Graph) == 16, "sizeof(Graph) == 24 bytes");
+
+static constexpr const uint16_t MAX_NODES_COUNT = std::numeric_limits<uint16_t>::max() - 1;
+
+
+void Node::Init(uint16_t id) {
+    m_id = id;
+}
+
+void Node::Clear(uint16_t nextFreeIndex) {
+    m_nextFreeIndex = nextFreeIndex;
+    m_data = nullptr;
+    if (m_pins != nullptr) {
+        delete[] m_pins;
+    }
+}
+
+uint16_t Node::Create(uint8_t countInputPins, uint8_t countOutputPins, void* data) {
+    uint16_t nextFreeIndex = m_nextFreeIndex;
+    m_countInputPins = countInputPins;
+    m_countOutputPins = countOutputPins;
+    m_pins = new Pin[countInputPins + countOutputPins];
+    m_data = data;
+    return nextFreeIndex;
+}
 
 Graph::Graph(uint16_t initialNodeCount)
     : m_free(initialNodeCount)
     , m_capacity(initialNodeCount)
-    , m_nodes(new Node[initialNodeCount]) {
+    , m_firstFree(0)
+    , m_nodes(new Node[m_capacity]) {
+
+    for (uint16_t i=0; i!=m_capacity; ++i) {
+        m_nodes[i].Init(i + 1);
+        m_nodes[i].Clear(i + 1);
+    }
 }
 
 Graph::~Graph() {
     if (m_nodes != nullptr) {
+        for (uint16_t i=0; i!=m_capacity; ++i) {
+            m_nodes[i].Clear(0);
+        }
         delete[] m_nodes;
     }
+}
+
+Node& Graph::AddNode(uint8_t countInputPins, uint8_t countOutputPins, void* data) {
+    if (m_free == 0) {
+        if (m_capacity == (MAX_NODES_COUNT - 1)) {
+            throw EngineError("gs:Graph:AddNode failed to add node, node limit exceeded");
+        }
+
+        auto* prevNodes = m_nodes;
+        auto prevCapacity = m_capacity;
+
+        m_capacity = MAX_NODES_COUNT;
+        if (static_cast<uint32_t>(prevCapacity) * uint32_t(2) < static_cast<uint32_t>(MAX_NODES_COUNT)) {
+            m_capacity = prevCapacity * 2;
+        }
+
+        m_free = m_capacity - prevCapacity;
+        m_firstFree = prevCapacity;
+
+        m_nodes = new Node[m_capacity];
+        std::memcpy(m_nodes, prevNodes, prevCapacity * sizeof(Node));
+        delete[] prevNodes;
+        for (uint16_t i=prevCapacity; i!=m_capacity; ++i) {
+            m_nodes[i].Init(i + 1);
+            m_nodes[i].Clear(i + 1);
+        }
+    }
+
+    m_firstFree = m_nodes[m_firstFree].Create(countInputPins, countOutputPins, data);
+    --m_free;
+
+    return m_nodes[m_firstFree];
 }
 
 }
