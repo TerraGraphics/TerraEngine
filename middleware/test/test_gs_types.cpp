e7e5502404f6aa0b13def5266929a5eee16ac4bb
ReanGD <reangd@yandex.ru>
2020-06-11 00:22:51 +0300

    add tests for MaxTypeId

---
+++ middleware/test/test_gs_types.cpp
@@ -0,0 +1,19 @@
+#include <string>
+
+#include "test/test.h"
+#include "middleware/gscheme/graph/gs_types.h"
+
+
+namespace {
+
+TEST(gsTypes, MaxTypeId) {
+    ASSERT_EQ(gs::TypeId::Float, gs::MaxTypeId(gs::TypeId::Float, gs::TypeId::Float));
+    ASSERT_EQ(gs::TypeId::Vector2f, gs::MaxTypeId(gs::TypeId::Float, gs::TypeId::Vector2f));
+    ASSERT_EQ(gs::TypeId::Generator3D, gs::MaxTypeId(gs::TypeId::Vector4f, gs::TypeId::Generator3D));
+
+    ASSERT_EQ(gs::TypeId::Float, gs::MaxTypeId(gs::TypeId::Float, gs::TypeId::UniversalFloat));
+    ASSERT_EQ(gs::TypeId::Vector2f, gs::MaxTypeId(gs::TypeId::Vector2f, gs::TypeId::UniversalFloat));
+    ASSERT_EQ(gs::TypeId::Generator3D, gs::MaxTypeId(gs::TypeId::UniversalVector4f, gs::TypeId::UniversalGenerator3D));
+}
+
+}
