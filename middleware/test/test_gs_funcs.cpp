a6c8326d44974c36f8801c6e5e04a2a2717cf1cc
ReanGD <reangd@yandex.ru>
2020-06-12 00:34:45 +0300

    add tests for funcAdd

---
+++ middleware/test/test_gs_funcs.cpp
@@ -0,0 +1,23 @@
+#include <string>
+
+#include "test/test.h"
+#include "middleware/gscheme/embedded/func_add.h"
+
+
+namespace {
+
+TEST(gsFuncs, FuncAdd) {
+    gs::FuncAdd f;
+    f.SetA(1.f);
+    f.SetB(2.f);
+
+    ASSERT_FLOAT_EQ(1.f, std::get<float>(f.GetA()));
+    ASSERT_FLOAT_EQ(2.f, std::get<float>(f.GetB()));
+    ASSERT_FLOAT_EQ(3.f, std::get<float>(f.Result()));
+
+    f.SetA(Eigen::Vector2f(3.f, 4.f));
+    ASSERT_EQ(Eigen::Vector2f(3.f, 4.f), std::get<Eigen::Vector2f>(f.GetA()));
+    ASSERT_EQ(Eigen::Vector2f(5.f, 6.f), std::get<Eigen::Vector2f>(f.Result()));
+}
+
+}
