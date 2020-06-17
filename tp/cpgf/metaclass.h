6b2ea108865452c660aa5f4ab43c5796e3df94ad
ReanGD <reangd@yandex.ru>
2020-06-17 03:06:19 +0300

    add wrappers for cpgf

---
+++ tp/cpgf/metaclass.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#include "cpgf/gmetaclass.h"
+#pragma GCC diagnostic pop
---
+++ tp/cpgf/metadefine.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#include "cpgf/gmetadefine.h"
+#pragma GCC diagnostic pop
