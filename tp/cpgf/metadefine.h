233f3c890726181878715d603adbbc5ef2d75d44
ReanGD <reangd@yandex.ru>
2020-06-19 02:13:43 +0300

    fix gcc compile errors

--- tp/cpgf/metadefine.h
+++ tp/cpgf/metadefine.h
@@ -4,5 +4,9 @@
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #pragma GCC diagnostic ignored "-Wold-style-cast"
 #pragma GCC diagnostic ignored "-Wsign-conversion"
+#if defined(__GNUC__)
+#pragma GCC diagnostic ignored "-Wpedantic"
+#pragma GCC diagnostic ignored "-Wredundant-decls"
+#endif
 #include "cpgf/gmetadefine.h"
 #pragma GCC diagnostic pop
