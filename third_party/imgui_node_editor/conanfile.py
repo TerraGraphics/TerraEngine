2c8b9543f4e9efe8d1e7479da4e730ada216adb0
ReanGD <reangd@yandex.ru>
2020-09-26 04:30:12 +0300

    update imgui node editor version

--- third_party/imgui_node_editor/conanfile.py
+++ third_party/imgui_node_editor/conanfile.py
@@ -1,36 +1,33 @@
-import os
 from conans import ConanFile, CMake, tools
 
 
 class ImguiNodeEditor(ConanFile):
     name = "imgui_node_editor"
-    commit_sha = "cf3a1c3"
+    commit_sha = "687a72f"
     version = f"0.9.{commit_sha}"
     license = "MIT License"
     url = "https://github.com/thedmd/imgui-node-editor"
     description = "Node Editor using ImGui "
     topics = ("node-editor")
     settings = "os", "compiler", "build_type", "arch"
-    options = { }
-    default_options = { }
+    options: dict = {}
+    default_options: dict = {}
     generators = "cmake"
     exports_sources = "CMakeLists.txt"
-    imgui = "imgui/1.77.docking.3d4af15@terragraphics/wip"
+    imgui = "imgui/1.78.docking.05bc204@terragraphics/wip"
 
     def source(self):
-        self.run(f"git clone {self.url} repo")
-        with tools.chdir("repo"):
+        self.run(f"git clone {self.url}")
+        with tools.chdir("imgui-node-editor"):
             self.run(f"git reset --hard {self.commit_sha}")
-        self.run("rm ./repo/NodeEditor/CMakeLists.txt")
-        self.run("cp -r ./repo/NodeEditor/* .")
+
+    def configure(self):
+        self.requires(self.imgui)
 
     def _create_cmake(self):
         cmake = CMake(self)
         cmake.configure()
         return cmake
-
-    def requirements(self):
-        self.requires(self.imgui)
 
     def build(self):
         cmake = self._create_cmake()
