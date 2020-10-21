e862fc082e4e451c45bb95c028a89221986b98d6
ReanGD <reangd@yandex.ru>
2020-10-22 02:53:25 +0300

    refactoring random generator

--- core/include/core/math/random.h
+++ core/include/core/math/random.h
@@ -10,42 +10,42 @@ extern std::mt19937_64 randomGenerator;
 
 void RandSeed(std::mt19937_64::result_type value);
 
-template<typename T> typename std::enable_if<std::is_integral<T>::value, T>::type inline Rand() {
+template<typename T> typename std::enable_if_t<std::is_integral_v<T>, T> inline Rand() {
     return std::uniform_int_distribution<T>()(randomGenerator);
 }
 
-template<typename T> typename std::enable_if<std::is_floating_point<T>::value, T>::type inline Rand() {
+template<typename T> typename std::enable_if_t<std::is_floating_point_v<T>, T> inline Rand() {
     return std::uniform_real_distribution<T>()(randomGenerator);
 }
 
-template<typename T> typename std::enable_if<std::is_same<T, dg::float2>::value, T>::type inline Rand() {
+template<typename T> typename std::enable_if_t<std::is_same_v<T, dg::float2>, T> inline Rand() {
     return T(Rand<float>(), Rand<float>());
 }
 
-template<typename T> typename std::enable_if<std::is_same<T, dg::float3>::value, T>::type inline Rand() {
+template<typename T> typename std::enable_if_t<std::is_same_v<T, dg::float3>, T> inline Rand() {
     return T(Rand<float>(), Rand<float>(), Rand<float>());
 }
 
-template<typename T> typename std::enable_if<std::is_same<T, dg::float4>::value, T>::type inline Rand() {
+template<typename T> typename std::enable_if_t<std::is_same_v<T, dg::float4>, T> inline Rand() {
     return T(Rand<float>(), Rand<float>(), Rand<float>(), Rand<float>());
 }
 
-template<typename T> typename std::enable_if<std::is_integral<T>::value, T>::type inline LinearRand(const T minValue, const T maxValue) {
+template<typename T> typename std::enable_if_t<std::is_integral_v<T>, T> inline LinearRand(const T minValue, const T maxValue) {
     return std::uniform_int_distribution<T>(minValue, maxValue)(randomGenerator);
 }
 
-template<typename T> typename std::enable_if<std::is_floating_point<T>::value, T>::type inline LinearRand(const T minValue, const T maxValue) {
+template<typename T> typename std::enable_if_t<std::is_floating_point_v<T>, T> inline LinearRand(const T minValue, const T maxValue) {
     return std::uniform_real_distribution<T>(minValue, maxValue)(randomGenerator);
 }
 
-template<typename T> typename std::enable_if<std::is_same<T, dg::float2>::value, T>::type inline LinearRand(const T& minValue, const T& maxValue) {
+template<typename T> typename std::enable_if_t<std::is_same_v<T, dg::float2>, T> inline LinearRand(const T& minValue, const T& maxValue) {
     return T(LinearRand<float>(minValue.x, maxValue.x), LinearRand<float>(minValue.y, maxValue.y));
 }
 
-template<typename T> typename std::enable_if<std::is_same<T, dg::float3>::value, T>::type inline LinearRand(const T& minValue, const T& maxValue) {
+template<typename T> typename std::enable_if_t<std::is_same_v<T, dg::float3>, T> inline LinearRand(const T& minValue, const T& maxValue) {
     return T(LinearRand<float>(minValue.x, maxValue.x), LinearRand<float>(minValue.y, maxValue.y), LinearRand<float>(minValue.z, maxValue.z));
 }
 
-template<typename T> typename std::enable_if<std::is_same<T, dg::float4>::value, T>::type inline LinearRand(const T& minValue, const T& maxValue) {
+template<typename T> typename std::enable_if_t<std::is_same_v<T, dg::float4>, T> inline LinearRand(const T& minValue, const T& maxValue) {
     return T(LinearRand<float>(minValue.x, maxValue.x), LinearRand<float>(minValue.y, maxValue.y), LinearRand<float>(minValue.z, maxValue.z), LinearRand<float>(minValue.w, maxValue.w));
 }
