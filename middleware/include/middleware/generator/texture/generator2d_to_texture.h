65283c27685ba051fbb2783c462d28d8919702fd
ReanGD <reangd@yandex.ru>
2020-10-27 00:25:47 +0300

    add converter from 2d generator to texture

---
+++ middleware/include/middleware/generator/texture/generator2d_to_texture.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "dg/dg.h"
+#include "core/math/types.h"
+#include "core/common/ctor.h"
+#include "core/math/generator_type.h"
+
+
+class DynamicTexture;
+class Generator2dToTexture : Fixed {
+public:
+    Generator2dToTexture();
+    ~Generator2dToTexture();
+
+    TexturePtr Result();
+
+    math::Size GetTextureSize() const { return m_textureSize; }
+    void SetTextureSize(const math::Size v);
+
+    math::RectF GetGeneratorRect() const { return m_generatorRect; }
+    void SetGeneratorRect(const math::RectF v);
+
+    math::Generator2d GetInput() const { return m_input; }
+    void SetInput(const math::Generator2d& v) { m_input = v; }
+
+private:
+    void FillTexture(dg::RefCntAutoPtr<DynamicTexture>& texture) const;
+
+private:
+    dg::RefCntAutoPtr<DynamicTexture> m_texture;
+    math::Size m_textureSize = math::Size(128, 128);
+    math::RectF m_generatorRect = math::RectF(0, 0, 128.f, 128.f);
+    math::Generator2d m_input;
+};
---
+++ middleware/src/generator/texture/generator2d_to_texture.cpp
@@ -0,0 +1,83 @@
+#include "middleware/generator/texture/generator2d_to_texture.h"
+
+#include "dg/device.h"
+#include "core/engine.h"
+#include "core/common/exception.h"
+#include "core/material/texture.h"
+#include "core/material/texture_manager.h"
+
+
+namespace {
+    bool IsCorrectTextureSize(math::Size size) {
+        if ((size.w == 0) || (size.h == 0)) {
+            return false;
+        }
+
+        if ((size.w == 1) && (size.h == 1)) {
+            return true;
+        }
+
+        if (((size.w % 4) == 0) && ((size.h % 4) == 0)) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+Generator2dToTexture::Generator2dToTexture() {
+
+}
+
+Generator2dToTexture::~Generator2dToTexture() {
+    m_texture.Release();
+}
+
+TexturePtr Generator2dToTexture::Result() {
+    if (!m_texture) {
+        m_texture = Engine::Get().GetTextureManager()->CreateDynamicTexture(
+            dg::TEX_FORMAT_RGBA8_UNORM, m_textureSize.w, m_textureSize.h, "tex::Generator2dToTexture");
+    } else {
+        m_texture->SetSize(m_textureSize.w, m_textureSize.h);
+    }
+
+    FillTexture(m_texture);
+
+    return m_texture->Get();
+}
+
+void Generator2dToTexture::SetTextureSize(const math::Size v) {
+    if (!IsCorrectTextureSize(v)) {
+        throw EngineError("Generator2dToTexture: texture size must be non-zero and multiple of 4 or equal to 1");
+    }
+    m_textureSize = v;
+}
+
+void Generator2dToTexture::SetGeneratorRect(const math::RectF v) {
+    if ((v.Width() <= 0) || (v.Height() <= 0)) {
+        throw EngineError("Generator2dToTexture: generator rectangle dimensions must be greater than zero");
+    }
+    m_generatorRect = v;
+}
+
+void Generator2dToTexture::FillTexture(dg::RefCntAutoPtr<DynamicTexture>& texture) const {
+    auto lock = texture->Lock(Engine::Get().GetContext());
+    double uDelta  = m_generatorRect.Width() / static_cast<double>(lock.width);
+    double vDelta  = m_generatorRect.Height() / static_cast<double>(lock.height);
+    double v = m_generatorRect.y;
+
+    for (uint32_t y=0; y!=lock.height; ++y) {
+        auto* pDest = reinterpret_cast<uint32_t*>(lock.data + lock.stride * y);
+        double u = m_generatorRect.x;
+        for (uint32_t x=0; x!=lock.width; ++x) {
+            double d = std::min(std::max((m_input(u, v) + 1.) * 255. * 0.5, 0.), 255.);
+            auto component = static_cast<uint8_t>(std::min(std::max(static_cast<int>(d), 0), 255));
+            *pDest = math::Color4(component, component, component).value;
+
+            // Go to the next point.
+            ++pDest;
+            u += uDelta;
+        }
+        v += vDelta;
+    }
+}
