8caafcc09fb3898f8b987708c796bb61813a398b
ReanGD <reangd@yandex.ru>
2020-11-08 01:06:33 +0300

    add MetaPrimitiveTypeProperty

---
+++ middleware/include/middleware/gscheme/meta/gs_meta_primitive_type_property.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <limits>
+#include <type_traits>
+
+
+namespace gs {
+
+template<typename T, typename Enable = std::enable_if_t<std::is_integral_v<T> || std::is_floating_point_v<T>>>
+struct MetaPrimitiveTypeProperty {
+    bool (*TCheckFunc)(T);
+
+public:
+    void Max(T maxValue) {
+        m_maxValue = maxValue;
+    }
+
+    void Min(T minValue) {
+        m_minValue = minValue;
+    }
+
+    void CheckFunc(const TCheckFunc& func) {
+        m_checkFunc = func;
+    }
+
+public:
+    T m_maxValue = std::numeric_limits<T>::max();
+    T m_minValue = std::numeric_limits<T>::min();
+    TCheckFunc m_checkFunc = nullptr;
+};
+
+}
