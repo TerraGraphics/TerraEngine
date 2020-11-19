6fce753974a91e76bada497d5b760637d83a8ef1
ReanGD <reangd@yandex.ru>
2020-11-19 04:44:36 +0300

    optimize combobox

--- middleware/src/imgui/combo_box.cpp
+++ middleware/src/imgui/combo_box.cpp
@@ -5,8 +5,11 @@
 
 #include "imgui/imgui.h"
 #include "imgui/internal.h"
-#include "imgui/gui_helpers.h"
+#include "imgui/widget_draw.h"
 #include "core/common/exception.h"
+#include "imgui/widget_placement.h"
+#include "middleware/imgui/button.h"
+#include "middleware/imgui/imgui_math.h"
 
 
 namespace gui {
@@ -18,7 +21,7 @@ static float CalcMaxPopupHeightFromItemCount(int items_count) {
     return (g.FontSize + g.Style.ItemSpacing.y) * static_cast<float>(items_count) - g.Style.ItemSpacing.y + (g.Style.WindowPadding.y * 2);
 }
 
-bool ComboBox(std::string_view /* strId */, size_t& index, const std::vector<std::string>& items, const ComboBoxStyle& /* style */, math::RectF* outWidgetRect) {
+bool ComboBox(std::string_view strId, size_t& index, const std::vector<std::string>& items, const ComboBoxStyle& style, math::RectF* outWidgetRect) {
     bool changed = false;
 
     ImGuiWindow* window = GetCheckedCurrentWindow(outWidgetRect);
@@ -29,31 +32,31 @@ bool ComboBox(std::string_view /* strId */, size_t& index, const std::vector<std
     if (index >= items.size()) {
         throw EngineError("gui::ComboBox: wrong index = {}, max value = {}", index, items.size() - 1);
     }
+    const char* previewValue = items[index].c_str();
 
-
-
-    const char label[] = "combobox";
-    ImGuiComboFlags flags = ImGuiComboFlags_None;
-    const char* preview_value = items[index].c_str();
-
-    // Always consume the SetNextWindowSizeConstraint() call in our early return paths
     ImGuiContext& g = *GImGui;
-    bool has_window_size_constraint = (g.NextWindowData.Flags & ImGuiNextWindowDataFlags_HasSizeConstraint) != 0;
+    const ImGuiID id = window->GetID(strId.cbegin(), strId.cend());
+    const auto drawSize = math::SizeF(style.width, GetDefaultFieldHeight());
+    // Always consume the SetNextWindowSizeConstraint() call in our early return paths
+    bool hasWindowSizeConstraint = (g.NextWindowData.Flags & ImGuiNextWindowDataFlags_HasSizeConstraint) != 0;
     g.NextWindowData.Flags &= ~ImGuiNextWindowDataFlags_HasSizeConstraint;
 
-    const ImGuiStyle& style = g.Style;
-    const ImGuiID id = window->GetID(label);
-
-    const float arrow_size = ImGui::GetFrameHeight();
-    const ImVec2 label_size = ImGui::CalcTextSize(label, NULL, true);
-    const float width = ImGui::CalcItemWidth();
-    // const float w = expected_w;
-    const ImRect frame_bb(window->DC.CursorPos, window->DC.CursorPos + ImVec2(width, label_size.y + style.FramePadding.y * 2.0f));
-    const ImRect total_bb(frame_bb.Min, frame_bb.Max + ImVec2(label_size.x > 0.0f ? style.ItemInnerSpacing.x + label_size.x : 0.0f, 0.0f));
-    ImGui::ItemSize(total_bb, style.FramePadding.y);
-    if (!ImGui::ItemAdd(total_bb, id, &frame_bb)) {
+    math::RectF drawRect;
+    math::RectF widgetRect;
+    bool res = PlaceWidget(id, &style, drawSize, &drawRect, &widgetRect);
+    if (outWidgetRect != nullptr) {
+        *outWidgetRect = widgetRect;
+    }
+    if (!res) {
         return changed;
     }
+
+    ImGuiComboFlags flags = ImGuiComboFlags_None;
+    const ImGuiStyle& gStyle = g.Style;
+
+    const float arrow_size = ImGui::GetFrameHeight();
+    const float width = drawSize.w;
+    const ImRect frame_bb = ToImGuiRect(drawRect);
 
     bool hovered, held;
     bool pressed = ImGui::ButtonBehavior(frame_bb, id, &hovered, &held);
@@ -62,39 +65,34 @@ bool ComboBox(std::string_view /* strId */, size_t& index, const std::vector<std
     const ImU32 frame_col = ImGui::GetColorU32(hovered ? ImGuiCol_FrameBgHovered : ImGuiCol_FrameBg);
     const float value_x2 = ImMax(frame_bb.Min.x, frame_bb.Max.x - arrow_size);
     ImGui::RenderNavHighlight(frame_bb, id);
-    window->DrawList->AddRectFilled(frame_bb.Min, ImVec2(value_x2, frame_bb.Max.y), frame_col, style.FrameRounding, ImDrawCornerFlags_Left);
+    window->DrawList->AddRectFilled(frame_bb.Min, frame_bb.Max, frame_col, gStyle.FrameRounding, ImDrawCornerFlags_Left);
     {
-        // ArrowButton
-        ImU32 bg_col = ImGui::GetColorU32((popup_open || hovered) ? ImGuiCol_ButtonHovered : ImGuiCol_Button);
-        ImU32 text_col = ImGui::GetColorU32(ImGuiCol_Text);
-        window->DrawList->AddRectFilled(ImVec2(value_x2, frame_bb.Min.y), frame_bb.Max, bg_col, style.FrameRounding, (width <= arrow_size) ? ImDrawCornerFlags_All : ImDrawCornerFlags_Right);
-        if (value_x2 + arrow_size - style.FramePadding.x <= frame_bb.Max.x)
-            ImGui::RenderArrow(window->DrawList, ImVec2(value_x2 + style.FramePadding.y, frame_bb.Min.y + style.FramePadding.y), text_col, ImGuiDir_Down, 1.0f);
+        if (value_x2 + arrow_size - gStyle.FramePadding.x <= frame_bb.Max.x) {
+            auto arrowDrawRect = drawRect - math::RectOffsetF(value_x2 - drawRect.x, 0, 0, 0);
+            DrawArrowIcon(window->DrawList, arrowDrawRect, GetThemeColor(ImGuiCol_Text), Direction::Down);
+        }
     }
-    ImGui::RenderFrameBorder(frame_bb.Min, frame_bb.Max, style.FrameRounding);
-    if (preview_value != NULL)
-        ImGui::RenderTextClipped(frame_bb.Min + style.FramePadding, ImVec2(value_x2, frame_bb.Max.y), preview_value, NULL, NULL, ImVec2(0.0f, 0.0f));
-    if (label_size.x > 0)
-        ImGui::RenderText(ImVec2(frame_bb.Max.x + style.ItemInnerSpacing.x, frame_bb.Min.y + style.FramePadding.y), label);
+    ImGui::RenderFrameBorder(frame_bb.Min, frame_bb.Max, gStyle.FrameRounding);
+    if (previewValue != nullptr) {
+        ImGui::RenderTextClipped(frame_bb.Min + gStyle.FramePadding, ImVec2(value_x2, frame_bb.Max.y), previewValue, NULL, NULL, ImVec2(0.0f, 0.0f));
+    }
 
-    if ((pressed || g.NavActivateId == id) && !popup_open)
-    {
-        if (window->DC.NavLayerCurrent == 0)
+    if ((pressed || g.NavActivateId == id) && !popup_open) {
+        if (window->DC.NavLayerCurrent == 0) {
             window->NavLastIds[0] = id;
+        }
         ImGui::OpenPopupEx(id, ImGuiPopupFlags_None);
         popup_open = true;
     }
 
-    if (!popup_open)
+    if (!popup_open) {
         return changed;
+    }
 
-    if (has_window_size_constraint)
-    {
+    if (hasWindowSizeConstraint) {
         g.NextWindowData.Flags |= ImGuiNextWindowDataFlags_HasSizeConstraint;
         g.NextWindowData.SizeConstraintRect.Min.x = ImMax(g.NextWindowData.SizeConstraintRect.Min.x, width);
-    }
-    else
-    {
+    } else {
         if ((flags & ImGuiComboFlags_HeightMask_) == 0)
             flags |= ImGuiComboFlags_HeightRegular;
         IM_ASSERT(ImIsPowerOfTwo(flags & ImGuiComboFlags_HeightMask_));    // Only one
@@ -109,9 +107,8 @@ bool ComboBox(std::string_view /* strId */, size_t& index, const std::vector<std
     ImFormatString(name, 16, "##Combo_%02d", g.BeginPopupStack.Size); // Recycle windows based on depth
 
     // Peak into expected window size so we can position it
-    if (ImGuiWindow* popup_window = ImGui::FindWindowByName(name))
-        if (popup_window->WasActive)
-        {
+    if (ImGuiWindow* popup_window = ImGui::FindWindowByName(name)) {
+        if (popup_window->WasActive) {
             ImVec2 size_expected = ImGui::CalcWindowExpectedSize(popup_window);
             if (flags & ImGuiComboFlags_PopupAlignLeft)
                 popup_window->AutoPosLastDirection = ImGuiDir_Left;
@@ -119,16 +116,16 @@ bool ComboBox(std::string_view /* strId */, size_t& index, const std::vector<std
             ImVec2 pos = ImGui::FindBestWindowPosForPopupEx(frame_bb.GetBL(), size_expected, &popup_window->AutoPosLastDirection, r_outer, frame_bb, ImGuiPopupPositionPolicy_ComboBox);
             ImGui::SetNextWindowPos(pos);
         }
+    }
 
     // We don't use BeginPopupEx() solely because we have a custom name string, which we could make an argument to BeginPopupEx()
     ImGuiWindowFlags window_flags = ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_Popup | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoMove;
 
     // Horizontally align ourselves with the framed text
-    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(style.FramePadding.x, style.WindowPadding.y));
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(gStyle.FramePadding.x, gStyle.WindowPadding.y));
     bool ret = ImGui::Begin(name, NULL, window_flags);
     ImGui::PopStyleVar();
-    if (!ret)
-    {
+    if (!ret) {
         ImGui::EndPopup();
         IM_ASSERT(0);   // This should never happen as we tested for IsPopupOpen() above
         return changed;
