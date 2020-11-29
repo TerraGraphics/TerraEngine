90e29b1fa6589d5b8ba64b900dd2719e12e12360
ReanGD <reangd@yandex.ru>
2020-11-29 04:07:55 +0300

    add new version of fastnoise to code

---
+++ tp/noise/fast_noise.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wimplicit-int-float-conversion"
+#include <FastNoiseLite.h>
+#pragma GCC diagnostic pop
